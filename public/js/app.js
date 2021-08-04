f3b574d661d7a617049c33e52540bff39e5c1a68
viana arroyo <arroyo.viana@gmail.com>
2021-08-04 09:45:55 +0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-const weatherForm = document.querySelector("form");
+git const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
